4b0c7011f7dbf924159848df5c3132161105b74f
Serge Titchenko <sergey.titchenko@gmail.com>
2020-03-18 17:26:26 +0400

    first fix

---
+++ index.js
@@ -0,0 +1,6 @@
+function getResult (depositAmount, mounthAmount, percent, depositTime);{
+document.getElementById(firsAmoun);
+document.getElementById(mounthAmount);
+document.getElementById(percent);
+document.getElementById(days);
+}
