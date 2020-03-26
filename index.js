269dec4294a6dfc67e6c7d2e21ce3e71c2dba164
Serge Titchenko <sergey.titchenko@gmail.com>
2020-03-26 14:16:48 +0400

    firs fix with js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-function getResult (depositAmount, mounthAmount, percent, depositTime);{
-document.getElementById(firsAmoun);
-document.getElementById(mounthAmount);
-document.getElementById(percent);
-document.getElementById(days);
+let button = document.getElementById("calculate");
+
+function calculateTotalAmount() {
+
+  let firsAmount = Number(document.getElementById("firsAmount").value);
+  let mounthAmount = Number(document.getElementById("mounthAmount").value);
+  let percent = Number(document.getElementById("percent").value);
+  let days = Math.floor(Number(document.getElementById("days").value) / 30);
+
+  if (isNaN(firsAmount) ||
+    isNaN(mounthAmount) ||
+    isNaN(percent) ||
+    isNaN(days) ||
+    percent >= 100 ||
+    percent <= 0 || 
+    mounthAmount < 0 || 
+    days <= 0 || 
+    firsAmount <= 0) {
+    console.error('Ошибка');
+    document.getElementById("miss").style.display = 'Block';
+    return NaN;
+  }
+  function summary(xfirsAmount, xmounthAmount, xpercent, xdays) {
+
+    totalNumber = (xfirsAmount + xmounthAmount) * (1 + (xpercent / 100 / 12) * xdays) + xmounthAmount * xdays;
+    // document.getElementById("miss").style.display = 'Block';
+    setTimeout(function () { alert(`Сумма на момент выплаты составит ` + totalNumber + ` Рублей`) }, 1);
+  }
+  summary(firsAmount, mounthAmount, percent, days);
 }
+button.addEventListener("click", calculateTotalAmount);
