e8f9dd67610f4ed529fc121da2c2c5b7dc633a0d
Serge Titchenko <sergey.titchenko@gmail.com>
2020-03-27 09:23:32 +0400

    gg

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 let button = document.getElementById("calculate");
 
-function calculateTotalAmount() {
+function calculateTotalAmount(firsAmount, mounthAmount, percent, days) {
+  document.getElementById('miss').style.display = 'none';
 
-  let firsAmount = Number(document.getElementById('firsAmount').value);
-  let mounthAmount = Number(document.getElementById('mounthAmount').value);
-  let percent = Number(document.getElementById('percent').value);
-  let days = Math.floor(Number(document.getElementById('days').value) / 30);
+  firsAmount = Number(document.getElementById('firsAmount').value);
+    mounthAmount = Number(document.getElementById('mounthAmount').value);
+    percent = Number(document.getElementById('percent').value);
+    days = Math.floor(Number(document.getElementById('days').value) / 30);
 
   if (isNaN(firsAmount) ||
     isNaN(mounthAmount) ||
     isNaN(percent) ||
     isNaN(days) ||
     percent >= 100 ||
-    percent <= 0 || 
-    mounthAmount < 0 || 
-    days <= 0 || 
+    percent <= 0 ||
+    mounthAmount < 0 ||
+    days <= 0 ||
     firsAmount <= 0) {
-    console.log('Nan');
+    console.error('Ошибка');
     document.getElementById('miss').style.display = 'Block';
-  
+    return NaN;
+  } else {
+    summary(firsAmount, mounthAmount, percent, days);
   }
-  function summary(rfirsAmount, rmounthAmount, rpercent, rdays) {
+}
+function summary(rfirsAmount, rmounthAmount, rpercent, rdays) {
 
-    totalNumber = (rfirsAmount + rmounthAmount) * (1 + (rpercent / 100 / 12) * rdays) + rmounthAmount * rdays;
-    setTimeout(function () { alert('Сумма на момент выплаты составит ' + Math.trunc(totalNumber) +  ' AZN') }, 1);
-  }
-  summary(firsAmount, mounthAmount, percent, days);
+  totalNumber = (rfirsAmount + rmounthAmount) * (1 + (rpercent / 100 / 12) * rdays) + rmounthAmount * rdays;
+
+  alert('Сумма на момент выплаты составит ' + Math.trunc(totalNumber) + ' AZN')
 }
 button.addEventListener('click', calculateTotalAmount);
